eab1c99b51b1625f635475bc4d376e3ffaee439b
Andrew Smithson <smithson@uk.ibm.com>
2019-07-23 21:46:04 +0000

    Switch positional arguments to flags

--- src/db2servicegen/main.go
+++ src/db2servicegen/main.go
@@ -29,15 +29,35 @@ type ServicesList struct {
 var app = cli.NewApp()
 
 func main() {
+  var server string
+  var userID string
+  var password string
+
   app.Name = "db2servicegen"
   app.Usage = "Generate service projects for Db2 REST Services"
+  app.Flags = []cli.Flag {
+    cli.StringFlag{
+      Name: "server, s",
+      Usage: "The Db2 server hostname and port",
+      Destination: &server,
+    },
+    cli.StringFlag{
+      Name: "user, u",
+      Usage: "The User ID for accessing the server",
+      Value: "",
+      Destination: &userID,
+    },
+    cli.StringFlag{
+      Name: "password, p",
+      Usage: "The password for accessing the server",
+      Value: "",
+      Destination: &password,
+    },
+  }
   app.Action = func(c *cli.Context) error {
-    if (c.NArg() < 1) {
-      return cli.NewExitError("No server URL specified", 1)
-    }
     client := &http.Client{}
-    req, _ := http.NewRequest("GET", c.Args().Get(0), nil)
-    req.SetBasicAuth(c.Args().Get(1), c.Args().Get(2))
+    req, _ := http.NewRequest("GET", server, nil)
+    req.SetBasicAuth(userID, password)
     req.Header.Add("Accept", "application/json")
     req.Header.Add("Content-Type", "application/json")
     resp, err := client.Do(req)
@@ -51,7 +71,7 @@ func main() {
     for _, service := range services.DB2Services {
       // fmt.Printf("%s\n", service.ServiceName)
       req, _ := http.NewRequest("GET", service.ServiceURL, nil)
-      req.SetBasicAuth(c.Args().Get(1), c.Args().Get(2))
+      req.SetBasicAuth(userID, password)
       req.Header.Add("Accept", "application/json")
       req.Header.Add("Content-Type", "application/json")
       resp, err := client.Do(req)
